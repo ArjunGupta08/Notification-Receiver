7d8163fded05c70457176259038cfa4b09657da9
Arjun Gupta <arjungupta0817@gmail.com>
2024-02-12 18:57:30 +0530

    Removing unnecessary CODE

--- app/src/main/java/com/android/mynotification/MainActivity.java
+++ app/src/main/java/com/android/mynotification/MainActivity.java
@@ -5,18 +5,8 @@ import static com.android.mynotification.Constants.TOPIC;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import com.android.mynotification.api.ApiUtilities;
-import com.google.android.material.button.MaterialButton;
-import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.messaging.FirebaseMessaging;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
     TextView title, message;
